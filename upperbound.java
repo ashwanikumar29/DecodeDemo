f8f406b7a120b08c20aa9bb74e0986524fbe7aa3
ashwanikumar29 <ashwanik2907@gmail.com>
2024-02-09 13:30:00 +0530

    First Commit

---
+++ upperbound.java
@@ -0,0 +1,22 @@
+public class upperbound {
+    public static void main(String[] args) {
+        int arr[] = { 10, 20, 30, 30, 40, 50, 60, 70 };
+
+        int x = 30;
+        int ans = 8;
+        int low = 0, high = arr.length - 1;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (x < arr[mid]) {
+                ans = Math.min(ans, mid);
+                high = mid - 1;
+            } else
+                low = mid + 1;
+        }
+
+        System.out.println("Upper bound for " + x + " is : " + ans);
+    }
+}
